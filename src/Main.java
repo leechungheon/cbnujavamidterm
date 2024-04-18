516b2ebbd204180faef300f5dd3853fa131f76f1
dawn_lch <chung4111@naver.com>
2024-04-19 00:03:08 +0900

    first

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
 public class Main {
-    public static void main(String[] args) {
-        Television myTv = new Television();
-        myTv.channel = 7;
-        myTv.volume = 9;
-        myTv.onOff = true;
-        int ch = myTv.getChannel();
-        System.out.println("현재 채널은 " + ch + "입니다.");
+    public static void main(String args[]) {
+        Point p = new Point(25, 78);
+        Circle c = new Circle(p, 10);
+        System.out.println(c);
     }
 }
