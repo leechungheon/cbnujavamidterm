7f32a9850bb90bbc90819b0303d13b6a83375ea6
dawn_lch <chung4111@naver.com>
2024-04-19 16:27:30 +0900

    first

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String args[]) {
-        Point p = new Point(25, 78);
-        Circle c = new Circle(p, 10);
-        System.out.println(c);
-    }
-}
