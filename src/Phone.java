f56bc7c7d45a36fe03d257a4f8bfa577ac7b47e0
dawn_lch <chung4111@naver.com>
2024-04-21 16:14:46 +0900

    first

--- src/Phone.java
+++
@@ -1,8 +0,0 @@
-public class Phone{
-    public String model;
-    public String color;
-
-    public Phone(){
-        System.out.println("Phone() 생성자 실행");
-    }
-}
--- src/SmartPhone.java
+++
@@ -1,9 +0,0 @@
-public class SmartPhone extends Phone{
-    public SmartPhone(String model, String color){
-        super();//생략가능
-        this.model=model;
-        this.color=color;
-        System.out.println("SmartPhone(String model, String color) 생성자 실행됨");
-
-    }
-}
